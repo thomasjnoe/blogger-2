5cddc496e7792065ce938ba2aa032fd98aeeb957
Thomas Noe <thomasjnoe@gmail.com>
2014-06-24 22:29:54 -0400

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   resources :tags
+
+  resources :authors
+
+  resources :author_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
